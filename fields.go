0069da1de344b8c406f9c7b286af5ee40acb0ef3
Sergey Klimkin <thy321@gmail.com>
2018-09-26 19:33:09 +0300

    bugfix

--- fields.go
+++ fields.go
@@ -4,15 +4,19 @@
 
 package fields
 
+import "bufio"
 import "strings"
 import "unicode"
+import "unicode/utf8"
 
-type Fields_t struct {
+// import "github.com/ondi/go-log"
+
+type fields_t struct {
 	Sep map[rune]int
 	last_quote rune
 }
 
-func (self * Fields_t) test(c rune) bool {
+func (self * fields_t) test(c rune) bool {
 	switch {
 	case self.last_quote == c:
 		self.last_quote = 0
@@ -28,22 +32,71 @@ func (self * Fields_t) test(c rune) bool {
 	}
 }
 
-func (self * Fields_t) Fields(in string) []string {
+func (self * fields_t) Fields(in string) []string {
 	return strings.FieldsFunc(in, self.test)
 }
 
-func TSV(in string) []string {
-	return (&Fields_t{Sep: map[rune]int{' ': 0, '\t': 0, '\v': 0, '\r': 0, '\n': 0, '\f': 0}}).Fields(in)
+func FieldsTSV(in string) []string {
+	return (&fields_t{Sep: map[rune]int{' ': 0, '\t': 0, '\v': 0, '\r': 0, '\n': 0, '\f': 0}}).Fields(in)
 }
 
-func CSV(in string) []string {
-	return (&Fields_t{Sep: map[rune]int{',': 0, ' ': 0, '\t': 0, '\v': 0, '\r': 0, '\n': 0, '\f': 0}}).Fields(in)
+func FieldsCSV(in string) []string {
+	return (&fields_t{Sep: map[rune]int{',': 0, ' ': 0, '\t': 0, '\v': 0, '\r': 0, '\n': 0, '\f': 0}}).Fields(in)
+}
+
+type Split_t struct {
+	Sep map[rune]int
+	Ignore map[rune]int
+}
+
+func (self * Split_t) Split(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	var last_rune rune
+	var last_size int
+	var last_quote rune
+	
+	for {
+		if last_rune, last_size = utf8.DecodeRune(data[advance:]); last_size == 0 {
+			break
+		}
+		// log.Debug("rune: '%c', last_size: %v", last_rune, last_size)
+		advance += last_size
+		
+		if last_quote == 0 && unicode.In(last_rune, unicode.Quotation_Mark) {
+			last_quote = last_rune
+		} else if last_quote == last_rune {
+			last_quote = 0
+		} else if _, ok := self.Sep[last_rune]; ok && last_quote == 0 {
+			if token == nil {
+				token = []byte{}
+			}
+			break
+		} else if _, ok := self.Ignore[last_rune]; ok && last_quote == 0 {
+			continue
+		} else {
+			token = append(token, data[advance - last_size:advance]...)
+		}
+	}
+	// log.Debug("RETURN: advance=%v, token='%s' (%v)", advance, token, token == nil)
+	return
+}
+
+func Split(in string) (res []string) {
+	scanner := bufio.NewScanner(strings.NewReader(in))
+	s := &Split_t {
+		Sep: map[rune]int{',': 0},
+		Ignore: map[rune]int{' ': 0, '\t': 0, '\v': 0, '\r': 0, '\n': 0, '\f': 0},
+	}
+	scanner.Split(s.Split)
+	for scanner.Scan() {
+		res = append(res, scanner.Text())
+	}
+	return
 }
 
 type Strings_t []string
 
 func (self * Strings_t) Set(value string) (err error) {
-	for _, v := range CSV(value) {
+	for _, v := range Split(value) {
 		*self = append(*self, v)
 	}
 	return
