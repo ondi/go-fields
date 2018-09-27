4844890b9b46914ef4bb81d1ab96f37aa7afbfef
Sergey Klimkin <thy321@gmail.com>
2018-09-27 15:21:29 +0300

    legacy reverted

--- fields.go
+++ fields.go
@@ -9,6 +9,39 @@ import "strings"
 import "unicode"
 import "unicode/utf8"
 
+type Fields_t struct {
+	Sep map[rune]int
+	last_quote rune
+}
+
+func (self * Fields_t) test(c rune) bool {
+	switch {
+	case self.last_quote == c:
+		self.last_quote = 0
+		return true		// false to keep quotes
+	case self.last_quote != 0:
+		return false
+	case unicode.In(c, unicode.Quotation_Mark):
+		self.last_quote = c
+		return true		// false to keep quotes
+	default:
+		_, ok := self.Sep[c]
+		return ok
+	}
+}
+
+func (self * Fields_t) Fields(in string) []string {
+	return strings.FieldsFunc(in, self.test)
+}
+
+func FieldsTSV(in string) []string {
+	return (&Fields_t {Sep: map[rune]int{' ': 0, '\t': 0, '\v': 0, '\r': 0, '\n': 0, '\f': 0}}).Fields(in)
+}
+
+func FieldsCSV(in string) []string {
+	return (&Fields_t{Sep: map[rune]int{',': 0, ' ': 0, '\t': 0, '\v': 0, '\r': 0, '\n': 0, '\f': 0}}).Fields(in)
+}
+
 type Split_t struct {
 	Sep map[rune]int
 	Ignore map[rune]int
@@ -35,7 +68,7 @@ func (self * Split_t) Split(data []byte, atEOF bool) (advance int, token []byte,
 				token = []byte{}
 			}
 			return
-		case self.Ignore[self.last_rune] != 0:
+		case self.Ignore[self.last_rune] != 0 && len(token) == 0:
 			//
 		default:
 			token = append(token, data[advance - self.last_size:advance]...)
@@ -44,7 +77,7 @@ func (self * Split_t) Split(data []byte, atEOF bool) (advance int, token []byte,
 	return
 }
 
-func split(in string, s * Split_t) (res []string) {
+func Split(in string, s * Split_t) (res []string) {
 	scanner := bufio.NewScanner(strings.NewReader(in))
 	scanner.Split(s.Split)
 	for scanner.Scan() {
@@ -56,17 +89,17 @@ func split(in string, s * Split_t) (res []string) {
 func SplitCSV(in string) []string {
 	s := &Split_t {
 		Sep: map[rune]int{',': 1},
-		Ignore: map[rune]int{' ': 1, '\t': 1, '\v': 1, '\r': 1, '\n': 1, '\f': 1},
+		Ignore: map[rune]int{'\v': 1, '\f': 1, '\r': 1, '\n': 1, '\t': 1, ' ': 1},
 	}
-	return split(in, s)
+	return Split(in, s)
 }
 
 func SplitTSV(in string) []string {
 	s := &Split_t {
 		Sep: map[rune]int{'\t': 1},
-		Ignore: map[rune]int{' ': 1, '\t': 1, '\v': 1, '\r': 1, '\n': 1, '\f': 1},
+		Ignore: map[rune]int{'\v': 1, '\f': 1, '\r': 1, '\n': 1, '\t': 1, ' ': 1},
 	}
-	return split(in, s)
+	return Split(in, s)
 }
 
 type Strings_t []string
