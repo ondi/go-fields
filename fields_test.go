9f41908c211b9eb442577693def5d824130e2229
Sergey Klimkin <thy321@gmail.com>
2018-10-02 23:12:40 +0300

    testing

--- fields_test.go
+++ fields_test.go
@@ -6,66 +6,70 @@ package fields
 
 import "testing"
 
-func Test01(t * testing.T) {
-	test := ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   "
-	res, err := SplitCSV(test)
-	if err != nil {
-		t.Logf("ERROR: %v", err)
-		return
-	}
-	t.Logf("test = %v", test)
-	t.Logf("res  = %#v", res)
-	if len(res) != 12 {
-		t.Fail()
-	}
+type Data_t struct {
+	In string
+	Out []string
+	Err bool
 }
 
-func Test02(t * testing.T) {
-	test := ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   ,"
-	res, err := SplitCSV(test)
-	if err != nil {
-		t.Logf("ERROR: %v", err)
-		return
-	}
-	t.Logf("test = %v", test)
-	t.Logf("res  = %#v", res)
-	if len(res) != 13 {
-		t.Fail()
-	}
+var data = []Data_t {
+	{
+		In: ",",
+		Out: []string{"", ""},
+	},
+	{
+		In: ",,",
+		Out: []string{"", "", ""},
+	},
+	{
+		In: " , , ",
+		Out: []string{"", "", ""},
+	},
+	{
+		In: "   1   ,",
+		Out: []string{"1", ""},
+	},
+	{
+		In: ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   ",
+		Out: []string{"", "", "',1,'", "", "' 2 '", "", "' ,3 ,'", "", "aaa4", "", "aaa5bbb", "this is test"},
+	},
+	{
+		In: ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   ,",
+		Out: []string{"", "", "',1,'", "", "' 2 '", "", "' ,3 ,'", "", "aaa4", "", "aaa5bbb", "this is test", ""},
+	},
+	{
+		In: ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   ,''",
+		Out: []string{"", "", "',1,'", "", "' 2 '", "", "' ,3 ,'", "", "aaa4", "", "aaa5bbb", "this is test", ""},
+	},
+	{
+		In: ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   ,'",
+		Out: []string{"", "", "',1,'", "", "' 2 '", "", "' ,3 ,'", "", "aaa4", "", "aaa5bbb", "this is test"},
+		Err: true,
+	},
 }
 
-func Test03(t * testing.T) {
-	test := ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   ,''"
-	res, err := SplitCSV(test)
-	if err != nil {
-		t.Logf("ERROR: %v", err)
-		return
-	}
-	t.Logf("test = %v", test)
-	t.Logf("res  = %#v", res)
-	if len(res) != 13 {
-		t.Fail()
-	}
-}
-
-func Test04(t * testing.T) {
-	test := ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   ,'"
-	_, err := SplitCSV(test)
-	if err == nil {
-		t.Fail()
-	}
-}
-
-func Test05(t * testing.T) {
-	test := ", , ',1,',,' 2 ', , ' ,3 ,', , aaa'4', ,aaa'5'bbb , this is test   ,"
-	res, err := SplitCSV(test)
-	if err != nil {
-		t.Logf("ERROR: %v", err)
-		return
-	}
-	t.Logf("test = %v", test)
-	t.Logf("res  = %#v", res)
-	if len(res) != 13 {
-		t.Fail()
+func Test001(t * testing.T) {
+	for _, v := range data {
+		res, err := SplitCSV(v.In)
+		t.Logf("In   = %v", v.In)
+		t.Logf("Out1 = %#v", v.Out)
+		t.Logf("Out2 = %#v", res)
+		if v.Err {
+			if err == nil {
+				t.Fatalf("NO ERROR FOUND")
+			}
+		} else {
+			if err != nil {
+				t.Fatalf("ERROR: %v", err)
+			}
+		}
+		if len(res) != len(v.Out) {
+			t.Fatalf("LENGTH")
+		}
+		for i, j := range res {
+			if j != v.Out[i] {
+				t.Fatalf("ELEMENT: '%v' != '%v'", j, v.Out[i])
+			}
+		}
 	}
 }
