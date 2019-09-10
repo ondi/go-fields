837a8299af831cb49bffc8fc875a425d9e0f7cab
ondi <thy321@gmail.com>
2019-09-10 21:23:19 +0300

    Split replaced with Set,Next

--- fields.go
+++ fields.go
@@ -8,7 +8,7 @@ import "strings"
 
 // import "github.com/ondi/go-log"
 
-type NextState func() NextState
+type NextState func() (NextState, bool)
 
 type Lexer_t struct {
 	sep map[rune]rune
@@ -16,10 +16,10 @@ type Lexer_t struct {
 	quote map[rune]rune
 	
 	reader * strings.Reader
+	state NextState
 	last_quote rune
 	last_token strings.Builder
 	last_trim strings.Builder
-	tokens []string
 	err error
 }
 
@@ -29,7 +29,7 @@ type Quote_t struct {
 }
 
 func NewLexer(sep []rune, trim []rune, quote []Quote_t) (self * Lexer_t) {
-	self = &Lexer_t{sep: map[rune]rune{}, trim: map[rune]rune{}, quote: map[rune]rune{}, tokens: []string{}}
+	self = &Lexer_t{sep: map[rune]rune{}, trim: map[rune]rune{}, quote: map[rune]rune{}}
 	for _, v := range sep {
 		self.sep[v] = 1
 	}
@@ -42,94 +42,107 @@ func NewLexer(sep []rune, trim []rune, quote []Quote_t) (self * Lexer_t) {
 	return
 }
 
-func (self * Lexer_t) Split(in string) ([]string, error) {
+func (self * Lexer_t) Set(in string) {
 	self.reader = strings.NewReader(in)
-	state := self.begin
-	for state != nil {
-		state = state()
-	}
-	return self.tokens, self.err
+	self.state = self.begin
 }
 
-func (self * Lexer_t) begin() NextState {
+func (self * Lexer_t) Next() (res string, ok bool) {
+	for self.state != nil && ok == false {
+		self.state, ok = self.state()
+	}
+	if ok {
+		res = self.last_token.String()
+		self.last_token.Reset()
+	}
+	return
+}
+
+func (self * Lexer_t) Err() error {
+	return self.err
+}
+
+func (self * Lexer_t) begin() (NextState, bool) {
 	last_rune, last_size, _ := self.reader.ReadRune()
 	// log.Debug("Begin: rune=`%c`, len=%v, tokens=%#v", last_rune, last_size, self.tokens)
 	switch {
 	case self.quote[last_rune] > 0:
 		self.last_quote = self.quote[last_rune]
-		return self.quoted
-	case self.trim[last_rune] > 0:
-		return self.begin
+		return self.quoted, false
 	case self.sep[last_rune] > 0:
-		self.tokens = append(self.tokens, self.last_token.String())
-		return self.begin
+		return self.begin, true
+	case self.trim[last_rune] > 0:
+		return self.begin, false
 	case last_size > 0:
 		self.last_token.WriteRune(last_rune)
-		return self.unquoted
+		return self.unquoted, false
 	default:
-		self.tokens = append(self.tokens, self.last_token.String())
-		return nil
+		return nil, true
 	}
 }
 
-func (self * Lexer_t) unquoted() NextState {
+func (self * Lexer_t) unquoted() (NextState, bool) {
 	last_rune, last_size, _ := self.reader.ReadRune()
 	// log.Debug("Unquoted: rune=`%c`, len=%v, tokens=%#v", last_rune, last_size, self.tokens)
 	switch {
+	case self.sep[last_rune] > 0:
+		self.last_trim.Reset()
+		return self.begin, true
 	case self.trim[last_rune] > 0:
 		self.last_trim.WriteRune(last_rune)
-		return self.unquoted
-	case self.sep[last_rune] > 0:
-		self.tokens = append(self.tokens, self.last_token.String())
-		self.last_token.Reset()
-		self.last_trim.Reset()
-		return self.begin
+		return self.unquoted, false
 	case last_size > 0:
 		if self.last_trim.Len() > 0 {
 			self.last_token.WriteString(self.last_trim.String())
 			self.last_trim.Reset()
 		}
 		self.last_token.WriteRune(last_rune)
-		return self.unquoted
+		return self.unquoted, false
 	default:
-		self.tokens = append(self.tokens, self.last_token.String())
-		return nil
+		return nil, true
 	}
 }
 
-func (self * Lexer_t) quoted() NextState {
+func (self * Lexer_t) quoted() (NextState, bool) {
 	last_rune, last_size, _ := self.reader.ReadRune()
 	// log.Debug("Quoted : rune=`%c`, len=%v, tokens=%#v", last_rune, last_size, self.tokens)
 	switch {
 	case self.last_quote == last_rune:
-		return self.unquoted
+		return self.unquoted, false
 	case last_size > 0:
 		self.last_token.WriteRune(last_rune)
-		return self.quoted
+		return self.quoted, false
 	default:
-		self.tokens = append(self.tokens, self.last_token.String())
-		return nil
+		return nil, true
 	}
 }
 
-func Split(in string, sep ...rune) ([]string, error) {
-	return NewLexer(sep,
+func Split(in string, sep ...rune) (res []string, err error) {
+	l := NewLexer(sep,
 		[]rune{'\v', '\f', '\r', '\n', '\t', ' '},
 		[]Quote_t{Quote_t{'"', '"'}, Quote_t{'\'', '\''}, Quote_t{'«', '»'}},
-	).Split(in)
+	)
+	l.Set(in)
+	for {
+		if token, ok := l.Next(); !ok {
+			break
+		} else {
+			res = append(res, token)
+		}
+	}
+	return res, l.Err()
 }
 
 type Strings_t []string
 
-func (self * Strings_t) Set(value string) (err error) {
-	var temp []string
-	if temp, err = Split(value, ','); err != nil {
-		return
+func (self * Strings_t) Set(value string) (error) {
+	temp, err := Split(value, ',')
+	if err != nil {
+		for _, v := range temp {
+			*self = append(*self, v)
+		}
 	}
-	for _, v := range temp {
-		*self = append(*self, v)
-	}
-	return
+	return err
 }
 
 func (self * Strings_t) String() string {
